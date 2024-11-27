829e4b0bf487161c3a81df6532319c5c5487eec2
焦少康 <2524385023@qq.com>
2024-11-27 20:23:55 +0800

    跳过多数据源

---
+++ 2.spring-boot-mybatis-MultiDataSource/src/test/java/com/cyy/multidatasource/SpringBootMybatisMultiDataSourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cyy.multidatasource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMybatisMultiDataSourceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
